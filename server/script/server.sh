ced638c8dd1b874d572bdf74cb540cfdcc63cc20
lvca <l.garulli@gmail.com>
2013-06-27 11:27:59 +0200

    Set JS Rhino optimization to the maximum in server script files. Forum reports 10x of improvement

--- server/script/server.sh
+++ server/script/server.sh
@@ -66,6 +66,6 @@ LOG_CONSOLE_LEVEL=info
 LOG_FILE_LEVEL=fine
 WWW_PATH=$ORIENTDB_HOME/www
 ORIENTDB_SETTINGS="-Dprofiler.enabled=true -Dcache.level1.enabled=false -Dcache.level2.strategy=1"
-JAVA_OPTS_SCRIPT="-XX:+HeapDumpOnOutOfMemoryError -Djava.awt.headless=true"
+JAVA_OPTS_SCRIPT="-XX:+HeapDumpOnOutOfMemoryError -Djava.awt.headless=true -Drhino.opt.level=9"
 
 $JAVA -server $JAVA_OPTS $JAVA_OPTS_SCRIPT $ORIENTDB_SETTINGS -Dfile.encoding=UTF8 -Djava.util.logging.config.file="$LOG_FILE" -Dorientdb.config.file="$CONFIG_FILE" -Dorientdb.www.path="$WWW_PATH" -Dorientdb.build.number="@BUILD@" -cp "$ORIENTDB_HOME/lib/orientdb-server-@VERSION@.jar:$ORIENTDB_HOME/lib/*" com.orientechnologies.orient.server.OServerMain
